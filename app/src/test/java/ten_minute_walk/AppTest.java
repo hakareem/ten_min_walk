558345ab3f1b15737195dcc3b294659273e2d34c
hakareem <harithakareem72@gmail.com>
2022-09-14 12:42:54 +0100

    forgot to add some testing

--- app/src/test/java/ten_minute_walk/AppTest.java
+++ app/src/test/java/ten_minute_walk/AppTest.java
@@ -4,7 +4,30 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class AppTest {
-    @Test public void appHasAGreeting() {
+    @Test public void testGivenACorrectList() {
         App test = new App();
+        char[] list = {'w', 's', 'e', 'e', 'n', 'n', 'e', 's', 'w', 'w'};
+
+        assertEquals(true, test.ten_minute_walk(list));
     }
+
+    @Test public void testGivenAWrongList() {
+        App test = new App();
+        char[] list = {'w', 's', 'e', 'n', 'n', 'e', 's', 'w', 'w', 'w'};
+
+        assertEquals(false, test.ten_minute_walk(list));
+    }
+
+    @Test public void testGivenAShortList() {
+        App test = new App();
+        char[] list = {'w', 's'};
+
+        assertEquals(false, test.ten_minute_walk(list));
+    }
+
 }
+
+
+// ten_minute_walk?(['w', 's', 'e', 'n', 'n', 'e', 's', 'w', 'w', 'w']) # => false
+// ten_minute_walk?(['w', 's', 'e', 's', 's', 'e', 's', 'w', 'n', 'n']) # => false
+// ten_minute_walk?(['w', 's']) # => false
