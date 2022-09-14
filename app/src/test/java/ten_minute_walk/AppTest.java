f1e3b5ebe6e009551c5c17a8bc1b63d017d1ef25
hakareem <harithakareem72@gmail.com>
2022-09-14 11:18:33 +0100

    no testing

---
+++ app/src/test/java/ten_minute_walk/AppTest.java
@@ -0,0 +1,10 @@
+package ten_minute_walk;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class AppTest {
+    @Test public void appHasAGreeting() {
+        App test = new App();
+    }
+}
